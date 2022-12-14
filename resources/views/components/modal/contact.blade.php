9a65b3a5e8b9f429af5097227079cec8000a49cc
reimiii <imiia75775@gmail.com>
2022-12-14 21:46:10 +0700

    Form modal, i dunno im never use modal before but yeah sure lest do it

--- resources/views/components/modal/contact.blade.php
+++ resources/views/components/modal/contact.blade.php
@@ -1,80 +1,84 @@
 <div {{ $attributes->class(['modal modal-blur fade']) }} id="modal-report" tabindex="-1" style="display: none;"
      aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Contact Mee</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="mb-3">
-                    <label class="form-label">Name</label>
-                    <input type="text" class="form-control" name="example-text-input" placeholder="Your report name"
-                           data-ddg-inputtype="identities.fullName">
+    <form action="" method="post">
+
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Contact Mee</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="row">
-                    <div class="col-lg-8">
-                        <div class="mb-3">
-                            <label class="form-label">Report url</label>
-                            <div class="input-group input-group-flat">
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label class="form-label">Name</label>
+                        <input type="text" class="form-control" name="example-text-input" placeholder="Your report name"
+                               data-ddg-inputtype="identities.fullName">
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <div class="mb-3">
+                                <label class="form-label">Report url</label>
+                                <div class="input-group input-group-flat">
                     <span class="input-group-text">
                       https://tabler.io/reports/
                     </span>
-                                <input type="text" class="form-control ps-0" value="report-01" autocomplete="off"
-                                       data-ddg-inputtype="unknown">
+                                    <input type="text" class="form-control ps-0" value="report-01" autocomplete="off"
+                                           data-ddg-inputtype="unknown">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="mb-3">
+                                <label class="form-label">Visibility</label>
+                                <select class="form-select" data-ddg-inputtype="unknown">
+                                    <option value="1" selected="">Private</option>
+                                    <option value="2">Public</option>
+                                    <option value="3">Hidden</option>
+                                </select>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-4">
-                        <div class="mb-3">
-                            <label class="form-label">Visibility</label>
-                            <select class="form-select" data-ddg-inputtype="unknown">
-                                <option value="1" selected="">Private</option>
-                                <option value="2">Public</option>
-                                <option value="3">Hidden</option>
-                            </select>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="mb-3">
+                                <label class="form-label">Client name</label>
+                                <input type="text" class="form-control" data-ddg-inputtype="unknown">
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="mb-3">
+                                <label class="form-label">Reporting period</label>
+                                <input type="date" class="form-control" data-ddg-inputtype="unknown">
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div>
+                                <label class="form-label">Additional information</label>
+                                <textarea class="form-control" rows="3"></textarea>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="mb-3">
-                            <label class="form-label">Client name</label>
-                            <input type="text" class="form-control" data-ddg-inputtype="unknown">
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="mb-3">
-                            <label class="form-label">Reporting period</label>
-                            <input type="date" class="form-control" data-ddg-inputtype="unknown">
-                        </div>
-                    </div>
-                    <div class="col-lg-12">
-                        <div>
-                            <label class="form-label">Additional information</label>
-                            <textarea class="form-control" rows="3"></textarea>
-                        </div>
-                    </div>
+                <div class="modal-footer">
+                    <a href="#" class="btn btn-link link-secondary" data-bs-dismiss="modal">
+                        Cancel
+                    </a>
+                    <a href="#" class="btn btn-primary ms-auto" data-bs-dismiss="modal">
+                        <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                             stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                             stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <line x1="12" y1="5" x2="12" y2="19"></line>
+                            <line x1="5" y1="12" x2="19" y2="12"></line>
+                        </svg>
+                        Submit
+                    </a>
                 </div>
-            </div>
-            <div class="modal-footer">
-                <a href="#" class="btn btn-link link-secondary" data-bs-dismiss="modal">
-                    Cancel
-                </a>
-                <a href="#" class="btn btn-primary ms-auto" data-bs-dismiss="modal">
-                    <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
-                         stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                         stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <line x1="12" y1="5" x2="12" y2="19"></line>
-                        <line x1="5" y1="12" x2="19" y2="12"></line>
-                    </svg>
-                    Create new report
-                </a>
             </div>
         </div>
-    </div>
+
+    </form>
 </div>
