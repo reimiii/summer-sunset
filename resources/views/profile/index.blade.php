23b88586ff365dd01da68050905cb28edd7259c9
reimiii <imiia75775@gmail.com>
2022-12-14 20:32:21 +0700

    Footer

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -7,5 +7,15 @@
         Profile
     </x-slot:header>
 
+    <div class="row justify-content-center">
+        <div class="col-lg-10 col-xl-9">
+            <div class="card card-lg">
+                <div class="card-body markdown">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 </x-app-layout>
